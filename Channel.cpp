312b76ec2c68aa246f635a3bb73deb4b4e47def9
mountassir-007 <ahmedelmountassir10@gmail.com>
2023-08-01 20:28:14 +0100

    code for the setters/getters and the adding function

--- Channel.cpp
+++ Channel.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Channel.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aadnane <aadnane@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ahel-mou <ahmed@1337.ma>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/31 16:47:40 by aadnane           #+#    #+#             */
-/*   Updated: 2023/08/01 16:19:27 by aadnane          ###   ########.fr       */
+/*   Updated: 2023/08/01 20:27:34 by ahel-mou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ Channel::Channel()
 Channel::Channel(std::string name, Client user)
 {
 	channelName = name;
-	channelOner = user.getNickname();
+	channelOwner = user.getNickname();
 	onlyInvited = NOT_SET;
 	channelIsPrivate = NOT_SET;
 	alreadyHasClients = HAS_CLIENTS;
@@ -43,20 +43,154 @@ Channel::~Channel()
 {
 }
 
-
 /*
 ** --------------------------------- OVERLOAD ---------------------------------
 */
 
-
 /*
 ** --------------------------------- METHODS ----------------------------------
 */
+//----------------------------------------------------------------- Getters
+std::string Channel::getChannelName() const
+{
+	return channelName;
+}
 
+std::string Channel::getChannelOwner() const
+{
+	return channelOwner;
+}
+
+std::string Channel::getChannelTopic() const
+{
+	return channelTopic;
+}
+
+std::vector<std::string> Channel::getChannelOperators() const
+{
+	return channelOperators;
+}
+
+std::vector<std::string> Channel::getInvitedList() const
+{
+	return invitedList;
+}
+
+int Channel::getOnlyInvited() const
+{
+	return onlyInvited;
+}
+
+int Channel::getChannelIsPrivate() const
+{
+	return channelIsPrivate;
+}
+
+int Channel::getAlreadyHasClients() const
+{
+	return alreadyHasClients;
+}
+
+int Channel::getOnlyOperatorMsg() const
+{
+	return onlyOperatorMsg;
+}
+
+size_t Channel::getMaxNumUsers() const
+{
+	return maxNumUsers;
+}
+
+size_t Channel::getUsersNum() const
+{
+	return usersNum;
+}
+//----------------------------------------------------------------- Setters
+
+void Channel::setChannelName(std::string channelName)
+{
+	this->channelName = channelName;
+}
+
+void Channel::setChannelOwner(std::string channelOwner)
+{
+	this->channelOwner = channelOwner;
+}
+
+void Channel::setChannelTopic(std::string channelTopic)
+{
+	this->channelTopic = channelTopic;
+}
+
+void Channel::setChannelOperators(const std::vector<std::string> &channelOperators)
+{
+	this->channelOperators = channelOperators;
+}
+
+void Channel::setInvitedList(const std::vector<std::string> &invitedList)
+{
+	this->invitedList = invitedList;
+}
+
+void Channel::setChannelPassword(std::string channelPassword)
+{
+	this->channelPassword = channelPassword;
+}
+
+void Channel::setOnlyInvited(int onlyInvited)
+{
+	this->onlyInvited = onlyInvited;
+}
+
+void Channel::setChannelIsPrivate(int channelIsPrivate)
+{
+	this->channelIsPrivate = channelIsPrivate;
+}
+
+void Channel::setAlreadyHasClients(int alreadyHasClients)
+{
+	this->alreadyHasClients = alreadyHasClients;
+}
+
+void Channel::setOnlyOperatorMsg(int onlyOperatorMsg)
+{
+	this->onlyOperatorMsg = onlyOperatorMsg;
+}
+
+void Channel::setMaxNumUsers(size_t maxNumUsers)
+{
+	this->maxNumUsers = maxNumUsers;
+}
+
+void Channel::setUsersNum(size_t usersNum)
+{
+	this->usersNum = usersNum;
+}
+
+//----------------------------------------------------------------- Methods
+
+bool Channel::addClientToChannel(Client user)
+{
+	if (usersNum >= maxNumUsers)
+	{
+		return false;
+	}
+
+	std::string nickname = user.getNickname();
+
+	if (channelClients.find(nickname) != channelClients.end())
+	{
+		return false;
+	}
+
+	channelClients[nickname] = user;
+	usersNum++;
+
+	return true;
+}
 
 /*
 ** --------------------------------- ACCESSOR ---------------------------------
 */
-
 
 /* ************************************************************************** */
