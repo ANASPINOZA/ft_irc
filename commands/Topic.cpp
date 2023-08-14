112aeb71b5b80d7c1525b5c34b321b27f71bddd4
mountassir-007 <ahmedelmountassir10@gmail.com>
2023-08-14 02:35:10 +0100

    now checking if there is only 2 parameters the topic command and the channel name also instead of printing the meedsage i send them to the client

--- commands/Topic.cpp
+++ commands/Topic.cpp
@@ -6,7 +6,7 @@
 /*   By: ahel-mou <ahmed@1337.ma>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/30 22:28:07 by ahel-mou          #+#    #+#             */
-/*   Updated: 2023/08/13 19:47:45 by ahel-mou         ###   ########.fr       */
+/*   Updated: 2023/08/14 02:33:53 by ahel-mou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,72 +14,66 @@
 
 void Topic(std::vector<std::string> cmd, Client &c, Server &s)
 {
-    if (cmd.size() < 3)
+    if (cmd.size() < 2)
     {
-        // Notify about missing parameters
-        std::cout << ERR_NEEDMOREPARAMS(c.getNickname()) << std::endl;
+        std::string errorMsg = ERR_NEEDMOREPARAMS(c.getNickname()) + "\r\n";
+        sendMessage(errorMsg, c.getFd());
         return;
     }
 
-    // Extract command parameters
     std::string channelName = cmd[1];
-    std::string topic = cmd[2];
+    std::string topic;
 
-    // Check if channel name starts with #
+    if (cmd.size() > 2)
+        topic = cmd[2];
+
     if (channelName[0] != '#')
     {
-        std::cout << ERR_NOSUCHCHANNEL(c.getNickname(), channelName) << std::endl;
+        std::string errorMsg = ERR_NOSUCHCHANNEL(c.getNickname(), channelName) + "\r\n";
+        sendMessage(errorMsg, c.getFd());
         return;
     }
-    if (s.isChannelIsThere(channelName) == false)
+
+    if (!s.isChannelIsThere(channelName))
     {
-        std::cout << ERR_NOSUCHCHANNEL(c.getNickname(), channelName) << std::endl;
+        std::string errorMsg = ERR_NOSUCHCHANNEL(c.getNickname(), channelName) + "\r\n";
+        sendMessage(errorMsg, c.getFd());
         return;
     }
 
     Channel channel = s.getChannelByName(channelName);
 
-    // Get the user's presence in the channel
     Client userInChannel = channel.getClientInChannel(c.getNickname());
-
-    // Check if the user is in the channel
     if (userInChannel.getNickname() != c.getNickname())
     {
-        std::cout << ERR_USERNOTINCHANNEL(c.getNickname(), channelName) << std::endl;
+        std::string errorMsg = ERR_USERNOTINCHANNEL(c.getNickname(), channelName) + "\r\n";
+        sendMessage(errorMsg, c.getFd());
         return;
     }
 
-    // Check if user has operator privileges
-    if (!channel.isOperator(c.getNickname()))
-    {
-        std::cout << ERR_CHANOPRIVSNEEDED(c.getNickname(), channelName) << std::endl;
-        return;
-    }
-
-    // Check if the topic is the same
-    if (channel.getChannelTopic() == topic)
-    {
-        std::cout << RPL_TOPIC(c.getNickname(), channelName, topic) << std::endl;
-        return;
-    }
-
-    // Set the new topic if it's not empty
     if (topic.empty())
     {
-        channel.setChannelTopic(topic);
-        std::cout << RPL_NOTOPIC(c.getNickname(), channelName) << std::endl;
+        std::string topicMsg = RPL_TOPIC(c.getNickname(), channelName, channel.getChannelTopic()) + "\r\n";
+        sendMessage(topicMsg, c.getFd());
     }
     else
     {
-        // Check if only operators can set topic
-        if (channel.getOnlyOperatorTopic() && !channel.isOperator(c.getNickname()))
+        if (!channel.isOperator(c.getNickname()))
         {
-            std::cout << ERR_CHANOPRIVSNEEDED(c.getNickname(), channelName) << std::endl;
+            std::string errorMsg = ERR_CHANOPRIVSNEEDED(c.getNickname(), channelName) + "\r\n";
+            sendMessage(errorMsg, c.getFd());
             return;
         }
 
-        // Set the new topic
+        if (channel.getChannelTopic() == topic)
+        {
+            std::string topicMsg = RPL_TOPIC(c.getNickname(), channelName, topic) + "\r\n";
+            sendMessage(topicMsg, c.getFd());
+            return;
+        }
+
         channel.setChannelTopic(topic);
-        std::cout << RPL_TOPIC(c.getNickname(), channelName, topic) << std::endl;
+        std::string topicMsg = RPL_TOPIC(c.getNickname(), channelName, topic) + "\r\n";
+        sendMessage(topicMsg, c.getFd());
     }
 }
