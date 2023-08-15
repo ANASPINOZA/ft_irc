ee385b121c34fadb36b092fa02d45cf0328bac4f
mountassir-007 <ahmedelmountassir10@gmail.com>
2023-08-15 16:17:21 +0100

    a global function to send msgs to a channel

--- msgHandler.cpp
+++ msgHandler.cpp
@@ -1,8 +1,18 @@
 #include "msgHandler.hpp"
 
-
-void sendMessage(const std::string& message, int fd) {
-    if (send(fd, message.c_str(), message.length(), 0) == -1) {
+void sendMessage(const std::string &message, const int fd)
+{
+    if (send(fd, message.c_str(), message.length(), 0) == -1)
+    {
         perror("send message error");
     }
+}
+
+void sendMessageToChannel(const Channel &channel, Client &sender, const std::string &message)
+{
+    for (auto &client : channel.getChannelClients())
+    {
+        sendMessage(message, client.second.getFd());
+    }
+    sendMessage(message, sender.getFd());
 }
